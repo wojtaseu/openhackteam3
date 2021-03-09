370e6d71988f2e70bd873bd84866e97723eb9199
Jon Lyall <JLyall@uk.ey.com>
2021-03-09 19:41:22 +0000

    added code

---
+++ FunctionAppOpenHack/HttpExample.cs
@@ -0,0 +1,34 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace FunctionAppOpenHack
+{
+    public static class HttpExample
+    {
+        [FunctionName("HttpExample")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["productId"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                 ? "" : $"The product name for your product id, {name} is Starfruit Explosion.";
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
